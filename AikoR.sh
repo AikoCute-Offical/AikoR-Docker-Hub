e7d878f1a5233b808aa2fe17a5c871b01ebe3580
Aiko <aikocute@icloud.com>
2022-07-29 23:58:10 +0700

    Update AikoR.sh

--- AikoR.sh
+++ AikoR.sh
@@ -26,6 +26,41 @@ else
     echo -e "${red}System version not detected, please contact the author!${plain}\n" && exit 1
 fi
 
+install_docker(){
+    if [[ ${release} == "centos" ]]; then
+yum install -y yum-utils
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io -y
+systemctl start docker
+systemctl enable docker
+
+    else
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+systemctl start docker
+systemctl enable docker
+    fi
+}
+
+install_docker_compose(){
+curl -fsSL https://get.docker.com | bash -s docker
+curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+}
+
 install() {
     if [[ -e /usr/local/AikoR/ ]]; then
         rm /usr/local/AikoR/ -rf
